fc913a26bbea9a9f5cd68f4d9f2f307383c8239a
David Alsh <alshdavid@gmail.com>
2026-04-06 12:31:50 +1000

    CI

--- .terraform/3-cloudflare.tf
+++ .terraform/3-cloudflare.tf
@@ -1,11 +1,7 @@
-data "aws_cloudfront_distribution" "s3_dist" {
-  id = var.cloudfront_distribution_id
-}
-
 resource "cloudflare_record" "deepscribe" {
   zone_id = var.cloudflare_zone_id
   name    = var.project_name
-  value   = aws_cloudfront_distribution.s3_distribution.domain_name
+  value   = "d2ffztt838w4th.cloudfront.net"
   type    = "CNAME"
   ttl     = 1
   proxied = true
