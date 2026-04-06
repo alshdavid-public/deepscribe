21f15757dfc4c615794fea47c72fc2c3fdfe5898
David Alsh <alshdavid@gmail.com>
2026-04-06 12:30:18 +1000

    CI

--- .terraform/3-cloudflare.tf
+++ .terraform/3-cloudflare.tf
@@ -1,3 +1,7 @@
+data "aws_cloudfront_distribution" "s3_dist" {
+  id = var.cloudfront_distribution_id
+}
+
 resource "cloudflare_record" "deepscribe" {
   zone_id = var.cloudflare_zone_id
   name    = var.project_name
