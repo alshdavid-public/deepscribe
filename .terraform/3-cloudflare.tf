a80c2f3b470368fff0bb8964c0beea39a0d1aa8e
David Alsh <alshdavid@gmail.com>
2026-04-06 12:32:44 +1000

    CI

--- .terraform/3-cloudflare.tf
+++ .terraform/3-cloudflare.tf
@@ -1,7 +1,7 @@
 resource "cloudflare_record" "deepscribe" {
   zone_id = var.cloudflare_zone_id
   name    = var.project_name
-  value   = "d2ffztt838w4th.cloudfront.net"
+  content   = "d2ffztt838w4th.cloudfront.net"
   type    = "CNAME"
   ttl     = 1
   proxied = true
